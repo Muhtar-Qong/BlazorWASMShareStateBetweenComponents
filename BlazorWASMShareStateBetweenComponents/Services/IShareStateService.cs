0df4dc2d8e4da13c485aa2044e5435272dca4800
mqnn12@yahoo.com <mqnn12@yahoo.com>
2021-07-07 15:41:49 -0400

    Add project files.

---
+++ BlazorWASMShareStateBetweenComponents/Services/IShareStateService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorWASMShareStateBetweenComponents.Services
+{
+    public interface IShareStateService
+    {
+        string Data { get; set; }
+        event Action OnChange;
+    }
+}
---
+++ BlazorWASMShareStateBetweenComponents/Services/ShareStateService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorWASMShareStateBetweenComponents.Services
+{
+    public class ShareStateService: IShareStateService
+    {
+        private string stateData = "";
+        public string Data
+        {
+            get => stateData;
+            set
+            {
+                stateData = value;
+                NotifyStateChanged();
+            }
+        }
+        public event Action OnChange;
+        void NotifyStateChanged() => OnChange?.Invoke();
+    }
+}
